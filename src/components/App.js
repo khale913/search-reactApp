66cecf2237fea0016750d7da98a278f3028868d6
Kelton Hale <keltonhale33@gmail.com>
2021-11-08 13:24:27 -0700

    fetched from api the images needed to load

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+
+  const onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID HX8rfS-T3USzdE3dSJ1erUnijUR6Mt86760sSIvX5mw",
+      },
+    });
+    setImages(response.data.results);
   };
 
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
       <SearchBar onSubmit={onSearchSubmit} />
+      Found: {images.length} images
     </div>
   );
 };
