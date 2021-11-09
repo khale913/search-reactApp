bfb1602b79bf38326d8838e366545e96c980da5d
Kelton Hale <keltonhale33@gmail.com>
2021-11-09 11:09:52 -0700

    added refs to image to fix sizing and order of photos with grid css

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const ImageCard = (props) => {
   const { description, urls } = props.image;
+  const [spanSize, setSpanSize] = useState(0);
+
+  const imageRef = useRef();
+
+  const imageLoaded = () => {
+    const height = imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    setSpanSize(spans);
+  };
 
   return (
-    <div>
-      <img alt={description} src={urls.regular} />
+    <div style={{ gridRowEnd: `span ${spanSize}` }}>
+      <img
+        ref={imageRef}
+        alt={description}
+        src={urls.regular}
+        onLoad={imageLoaded}
+      />
     </div>
   );
 };
