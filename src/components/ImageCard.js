751843f7cde5c146249f24181507533926465f9c
Kelton Hale <keltonhale33@gmail.com>
2021-11-09 09:28:48 -0700

    created imageCard component for images loaded

---
+++ src/components/ImageCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImageCard = (props) => {
+  const { description, urls } = props.image;
+
+  return (
+    <div>
+      <img alt={description} src={urls.regular} />
+    </div>
+  );
+};
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,19 +1,15 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
   console.log(props.images);
 
   const images = props.images.map((image) => {
-    return (
-      <img
-        src={image.urls.regular}
-        key={image.id}
-        alt={image.alt_description}
-      />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
